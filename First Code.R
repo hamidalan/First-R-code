a41b083ec29586ad9b15be49fc44c01b839ae7bc
HAMIDA ABDUL-KHALIQ <77174969+hamidalan@users.noreply.github.com>
2022-06-11 00:37:05 -0500

    Create First Code.R

---
+++ First Code.R
@@ -0,0 +1,15 @@
+#Installed the palmerpenguins to view the data
+install.packages('palmerpenguins')
+
+#you have to call the library before you can view the dataset
+library(palmerpenguins)
+
+#this shows the dataset penguins
+View(penguins)
+
+#Plotting a dummy graph
+plot(1:10)
+
+#Trying Hello World
+'Hello World'
+print('Hello World')
